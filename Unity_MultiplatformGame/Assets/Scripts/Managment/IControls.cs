25653bedf9db34b3d6fcc08a44bf36199dbe073d
Chantal <c_voort1992@hotmail.com>
2019-12-02 12:08:30 +0100

    Upload folder structure n basic scripts

---
+++ Unity_MultiplatformGame/Assets/Scripts/Managment/IControls.cs
@@ -0,0 +1,10 @@
+﻿public interface IControls {
+	//movement of the player
+	void Movement();
+
+	//breaks objects
+	void Shoot();
+
+	//enter portals
+	void Connect();
+}
---
+++ Unity_MultiplatformGame/Assets/Scripts/Managment/WebControls.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebControls : MonoBehaviour, IControls {
+	public void Movement() {
+		throw new System.NotImplementedException();
+	}
+
+	public void Shoot() {
+		throw new System.NotImplementedException();
+	}
+
+	public void Connect() {
+		throw new System.NotImplementedException();
+	}
+}
